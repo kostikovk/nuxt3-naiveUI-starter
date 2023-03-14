326baeb5c807433c8f09647576965d05fe866ad6
Kostiantyn Kostikov <kostiantyn.kostikov@titanbay.com>
2023-03-14 23:37:31 +0200

    fix: 🐛 naive-ui config

--- plugins/naive-ui.ts
+++ plugins/naive-ui.ts
@@ -2,32 +2,30 @@ import { setup } from '@css-render/vue3-ssr'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) {
+  if (process.server && nuxtApp.ssrContext) {
     const { collect } = setup(nuxtApp.vueApp)
     const originalRenderMeta = nuxtApp.ssrContext?.renderMeta
 
-    if (nuxtApp.ssrContext) {
-      nuxtApp.ssrContext.renderMeta = () => {
-        if (!originalRenderMeta) {
-          return {
-            headTags: collect()
-          }
+    nuxtApp.ssrContext.renderMeta = () => {
+      if (!originalRenderMeta) {
+        return {
+          headTags: collect()
         }
+      }
 
-        const originalMeta = originalRenderMeta()
+      const originalMeta = originalRenderMeta()
 
-        if ('then' in originalMeta) {
-          return originalMeta.then((resolvedOriginalMeta) => {
-            return {
-              ...resolvedOriginalMeta,
-              headTags: resolvedOriginalMeta.headTags + collect()
-            }
-          })
-        } else {
+      if ('then' in originalMeta) {
+        return originalMeta.then((resolvedOriginalMeta) => {
           return {
-            ...originalMeta,
-            headTags: originalMeta.headTags + collect()
+            ...resolvedOriginalMeta,
+            headTags: resolvedOriginalMeta.headTags + collect()
           }
+        })
+      } else {
+        return {
+          ...originalMeta,
+          headTags: originalMeta.headTags + collect()
         }
       }
     }
